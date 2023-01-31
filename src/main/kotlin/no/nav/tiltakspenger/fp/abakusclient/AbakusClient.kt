c04f6c42e864a0d9532ffe79207bb28fd11b230f
Richard Martinsen <github@martinsen.org>
2023-01-31 15:48:48 +0100

    Legg til logging og test med å rulle tilbake dependabot

--- src/main/kotlin/no/nav/tiltakspenger/fp/abakusclient/AbakusClient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/fp/abakusclient/AbakusClient.kt
@@ -17,9 +17,7 @@ import no.nav.tiltakspenger.fp.Configuration
 import no.nav.tiltakspenger.fp.abakusclient.models.Periode
 import no.nav.tiltakspenger.fp.abakusclient.models.Person
 import no.nav.tiltakspenger.fp.abakusclient.models.Request
-import no.nav.tiltakspenger.fp.abakusclient.models.Request2
 import no.nav.tiltakspenger.fp.abakusclient.models.YtelseV1
-import no.nav.tiltakspenger.fp.abakusclient.models.Ytelser
 import no.nav.tiltakspenger.fp.abakusclient.models.YtelserInput
 import no.nav.tiltakspenger.fp.defaultHttpClient
 import no.nav.tiltakspenger.fp.defaultObjectMapper
@@ -64,24 +62,25 @@ class AbakusClient(
 
     suspend fun hentYtelserv2(ident: String, fom: LocalDate, tom: LocalDate, behovId: String): List<YtelseV1> {
         SECURELOG.info { getToken }
-        val httpResponse =
-            httpClient.preparePost("${config.baseUrl}/fpabakus/ekstern/api/ytelse/v1/hent-vedtatte/hent-ytelse-vedtak") {
-                header(navCallIdHeader, behovId)
-                bearerAuth(getToken())
-                accept(ContentType.Application.Json)
-                contentType(ContentType.Application.Json)
-                setBody(
-                    Request2(
-                        person = Person(ident = ident),
-                        periode = Periode(fom = fom, tom = tom),
-                        ytelser = Ytelser.values().toList()
-                    )
-                )
-            }.execute()
-        return when (httpResponse.status) {
-            HttpStatusCode.OK -> httpResponse.call.response.body()
-            else -> throw RuntimeException("error (responseCode=${httpResponse.status.value}) from Abakus")
-        }
+        return emptyList()
+//        val httpResponse =
+//            httpClient.preparePost("${config.baseUrl}/fpabakus/ekstern/api/ytelse/v1/hent-vedtatte/hent-ytelse-vedtak") {
+//                header(navCallIdHeader, behovId)
+//                bearerAuth(getToken())
+//                accept(ContentType.Application.Json)
+//                contentType(ContentType.Application.Json)
+//                setBody(
+//                    Request2(
+//                        person = Person(ident = ident),
+//                        periode = Periode(fom = fom, tom = tom),
+//                        ytelser = Ytelser.values().toList()
+//                    )
+//                )
+//            }.execute()
+//        return when (httpResponse.status) {
+//            HttpStatusCode.OK -> httpResponse.call.response.body()
+//            else -> throw RuntimeException("error (responseCode=${httpResponse.status.value}) from Abakus")
+//        }
     }
 
     data class AbakusClientConfig(
